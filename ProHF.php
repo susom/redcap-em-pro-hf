ea73701e754e35cd28edd65b7a19a00447b1e21f
yasukawa <LYasukawa@stanfordhealthcare.org>
2021-08-24 18:03:11 -0700

    Fix URL that cron uses to move appointments

--- ProHF.php
+++ ProHF.php
@@ -13,17 +13,17 @@ class ProHF extends \ExternalModules\AbstractExternalModule {
 		// Other code to run when object is instantiated
 	}
 
-
     public function moveApptDataToRegistry() {
         // Only enabled on 1 project.  The apptDataToRegistry won't process anything unless it is the
         // correct appointment project. The apptDataToRegistry.php page will check to make sure it the pid is correct.
+        $this->emDebug("In moveApptDataToRegistry cron job with prefix: " . $this->PREFIX);
         $enabled = ExternalModules::getEnabledProjects($this->PREFIX);
         while($row = $enabled->fetch_assoc()){
 
             $proj_id = $row['project_id'];
 
             // Put together a URL so we get into project context
-            $this_url = $this->getUrl('pages/apptDataToRegistry.php', true, true) . '?pid=' . $proj_id;
+            $this_url = $this->getUrl('pages/apptDataToRegistry.php', true, true) . '&pid=' . $proj_id;
             $this->emDebug("This is the cron URL: " . $this_url);
 
             // Go into project context and process data for this project
